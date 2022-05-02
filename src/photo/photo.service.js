95d34ee2f3dc010ffca5d24d88ed2492c399d01c
DO TRONG PHUOC 20182724 <phuoc.dt182724@sis.hust.edu.vn>
2022-05-02 09:10:44 +0700

    done: update & delete a photo

--- src/photo/photo.service.js
+++ src/photo/photo.service.js
@@ -2,27 +2,36 @@ const db = require('../config/db.connection');
 const Photo = db.photo;
 const User = db.user;
 const Album = db.album;
-
+const {unlink} = require('fs')
 const createOne = async (userId, albumId, name, link) => {
- const [user, album] = await Promise.all([
-     User.findByPk(userId),
-     Album.findByPk(albumId)
- ]);
- const photo = await Photo.create({name, link});
- const rs1 =  user.addPhoto(photo);
- const rs2 =  album.addPhoto(photo);
+    const [user, album] = await Promise.all([
+        User.findByPk(userId),
+        Album.findByPk(albumId)
+    ]);
+    const photo = await Photo.create({ name, link });
+    const rs1 = user.addPhoto(photo);
+    const rs2 = album.addPhoto(photo);
     return photo;
-    // return res.status(StatusCodes.CREATED).json({rs1,rs2});
 }
 const getOne = async (id) => {
     const result = await Photo.findByPk(id);
     return result;
 }
-const updateOne = async () => {
-
+const updateOne = async (id, name) => {
+    const photo = await getOne(id);
+    photo.name = name;
+    photo.save();
+    return photo;
 }
-const deleteOne = async () => {
-
+const deleteOne = async (id) => {
+    const photo = await getOne(id);
+    // unlink(photo.link, (err) => {
+    //     if (err) throw err;
+    //     console.log(`${photo.name} was deleted!`);
+    //   });
+    const rs = await photo.destroy();
+    console.log(rs);
+    return rs;
 }
 
 
